0cb1e758f6aab03d1fcc097ab9291c2327786b04
yun mi Jang <newapache@naver.com>
2017-05-08 02:55:53 +0000

    askk

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,37 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all.reverse
+  end
+  
+  def write
+    post = Post.new
+    post.title = params[:title]
+    post.content = params[:content]
+    post.save
+   redirect_to '/'
+  end
+
+  def update
+    @one_post = Post.find(params[:p_id])
+  end
+  
+  def update_post
+    one_post = Post.find(params[:p_id])
+    one_post.title = params[:title]
+    one_post.content = params[:content]
+    one_post.save
+    
+    redirect_to '/'
+    
+  end
+  
+  
+  def destroy
+    
+    one_post = Post.find(params[:p_id])
+    one_post.destroy
+    
+     redirect_to '/'
+  end
+  
+end
